951fc0c0eee64bb7d33399d4311ae6adb9f6d0d7
Nihat Garibli <150590673+nihatgaribli@users.noreply.github.com>
2023-12-29 08:54:35 +0400

    ITBarins
    C# task's

---
+++ task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //klaviaturadan ədəd daxil edir
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("n ededi cüt ededdir.");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("n ededi ne tek ededdir, ne de cüt edededir.");
+            }
+            else
+            {
+                Console.WriteLine("n ededi tek ededdir.");
+            }
+
+        }
+    }
+}
---
+++ task2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _27._12._2023_tapşırıq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // klaviaturadan ədəd daxil edirik
+            if (n >= 0 && n <= 100)                      // ededin 0 - 100 aralığında olmağını yoxlayır
+            {
+                if (n > 90 && n <= 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("A");
+                }
+                else if (n > 80 && n <= 90)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("B");
+                }
+                else if (n > 70 && n <= 80)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("C");
+                }
+                else if (n > 60 && n <= 70)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("D");
+                }
+                else if (n > 50 && n <= 60)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("E");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("F");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmazsa, 0 - 100 araliginda eded daxil edin.");
+            }
+        }
+    }
+}
